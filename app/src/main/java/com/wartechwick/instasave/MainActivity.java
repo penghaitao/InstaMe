2898882a0ba885f867415625bef265d68b4aa91f
penghaitao <penghaitao19860226@gmail.com>
2016-01-08 16:51:41 +0000

    test

--- app/src/main/java/com/wartechwick/instasave/MainActivity.java
+++ app/src/main/java/com/wartechwick/instasave/MainActivity.java
@@ -66,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
